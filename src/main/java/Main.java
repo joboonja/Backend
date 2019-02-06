adfb40c7254fd0ef578d1edb926f3b1c3bb1e002
Farzad Habibi <farzadhabibii98@gmail.com>
2019-02-06 11:47:12 +0330

    add CommandHandler class and getCommand Method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,16 @@
-public class Main {
+import javafx.util.Pair;
+import org.json.JSONObject;
+import tools.CommandHandler;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[])
+    {
+        Pair<String, JSONObject> pairedCommand = CommandHandler.getCommand();
+        System.out.println(pairedCommand.getKey());
+        System.out.println(pairedCommand.getValue().toString());
+
+    }
 }
+
