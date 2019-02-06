ad55f071600672b4e08b220f168d73bf6593ebd5
Yasaman <ys.jafari98@gmail.com>
2019-02-06 11:45:01 +0330

    add constant values - for commands and user class implementation begins

---
+++ src/main/java/skill/Skill.java
@@ -0,0 +1,13 @@
+package skill;
+
+public class Skill {
+
+    private String name;
+    private int points;
+
+
+    Skill(String _name, int _points){
+        name = _name;
+        points = _points;
+    }
+}
---
+++ src/main/java/user/User.java
@@ -0,0 +1,26 @@
+package user;
+
+import config.Commands;
+import org.json.JSONObject;
+import skill.Skill;
+
+public class User {
+    private String name;
+    private Skill[] skills;
+
+    User(JSONObject userInfo){
+
+    }
+
+    public static void main(String[] args) {
+//        JSONObject obj = new JSONObject();
+//
+//        obj.put("name", "foo");
+//        obj.put("num", new Integer(100));
+//        obj.put("balance", new Double(1000.21));
+//        obj.put("is_vip", new Boolean(true));
+//
+//        System.out.print(Commands.AUCTION);
+    }
+
+}
