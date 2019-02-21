b56ca49a829c085748b9556225c0efaa581da375
Yasaman <ys.jafari98@gmail.com>
2019-02-21 17:24:33 +0330

    add implementation of getting data from remote http server.

---
+++ src/main/java/config/RemoteURLs.java
@@ -0,0 +1,5 @@
+package config;
+
+public class RemoteURLs {
+    public static final String BASE = "http://142.93.134.194:8000/joboonja/";
+}
--- src/main/java/tools/HttpRequest.java
+++ src/main/java/tools/HttpRequest.java
@@ -1,4 +1,31 @@
 package tools;
 
+import config.RemoteURLs;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HttpRequest {
+    private JSONObject getRemoteData() throws Exception {
+        String baseRemotURL = RemoteURLs.BASE;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(baseRemotURL).openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int status = connection.getResponseCode();
+
+        BufferedReader dataReader = new BufferedReader(status > 299 ? new InputStreamReader(connection.getErrorStream()) : new InputStreamReader(connection.getInputStream()));
+        String line;
+        StringBuffer response = new StringBuffer();
+
+        while ((line = dataReader.readLine()) != null) {
+            response.append(line);
+        }
+        dataReader.close();
+        return new JSONObject(response);
+    }
 }
