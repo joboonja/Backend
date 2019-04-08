55d6f0fb2bfd7f91569e1ff69df1600eb0dd0152
Farzad Habibi <farzadhabibii98@gmail.com>
2019-04-08 12:56:13 +0430

    This commit will update profile pic

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -11,5 +11,5 @@ public class UserConfig {
     public static final String USER_NOT_FOUND_ERROR = "UserInfo not found.";
     public static final String USERNAME_ALREADY_EXISTS_ERROR = "This username already exists.";
     public static final String DEFAULT_PROFILE_PIC_URL = "";
-    public static final String MAIN_USER_PROFILE_PIC_URL = "https://www.morpht.com/sites/morpht/files/styles/landscape/public/dalibor-matura_1.jpg?itok=gxCAhwAV";
+    public static final String MAIN_USER_PROFILE_PIC_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfK-MRS_WfCulpXZhOatZkat0IU-c7l7Zc-g0GJ4ZdLLLoTOb9";
 }
