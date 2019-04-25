b8073062c08a7665ff103512b158cc5ab801cf5d
Farzad Habibi <farzadhabibii98@gmail.com>
2019-04-25 15:56:24 +0430

    setup sqlite

--- src/main/java/models/data/connectionPool/ConnectionPool.java
+++ src/main/java/models/data/connectionPool/ConnectionPool.java
@@ -3,30 +3,22 @@ package models.data.connectionPool;
 import config.ConnectionPoolConfig;
 import org.apache.commons.dbcp.BasicDataSource;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConnectionPool {
-    private static ConnectionPool ourInstance = new ConnectionPool();
 
-    public static ConnectionPool getInstance() {
-        return ourInstance;
+    private static BasicDataSource ds = new BasicDataSource();
+
+    static {
+        ds.setUrl(ConnectionPoolConfig.url);
+        ds.setDriverClassName(ConnectionPoolConfig.driver);
+        ds.setMinIdle(ConnectionPoolConfig.minPools);
+        ds.setMaxIdle(ConnectionPoolConfig.maxPools);
     }
-    private DataSource dataSource;
-    private ConnectionPool() {
-        this.dataSource = this.setupDataSource();
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
     }
-    private DataSource setupDataSource() {
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName(ConnectionPoolConfig.driver);
-        basicDataSource.setUrl(ConnectionPoolConfig.url);
-        basicDataSource.setMinIdle(ConnectionPoolConfig.minPools);
-        basicDataSource.setMaxIdle(ConnectionPoolConfig.maxPools);
-        basicDataSource.setMaxOpenPreparedStatements(ConnectionPoolConfig.maxOpenPreparedStatement);
-        return basicDataSource;
-    }
-    public Connection getConnection() throws SQLException {
-        return this.dataSource.getConnection();
-    }
+
 }
