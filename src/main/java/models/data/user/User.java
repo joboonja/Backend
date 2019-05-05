6b033838722550c439d447d2ae1711b46ed4f002
Yasaman <ys.jafari98@gmail.com>
2019-05-05 20:03:16 +0430

    fix a bug in adding new skill for user

--- src/main/java/models/data/user/User.java
+++ src/main/java/models/data/user/User.java
@@ -5,8 +5,11 @@ import config.UserConfig;
 import exceptions.DuplicateEndorse;
 import exceptions.DuplicateSkill;
 import models.data.skill.UserSkill;
+import models.data.skill.mapper.UserSkillMapper.UserSkillMapper;
+import models.data.user.mapper.UserMapper;
 import models.services.project.ProjectService;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -33,11 +36,11 @@ public class User {
         this.bio = bio;
         this.profilePictureURL = id == ProjectServiceConfig.USER_ID ? UserConfig.MAIN_USER_PROFILE_PIC_URL : UserConfig.DEFAULT_PROFILE_PIC_URL;
     }
-    public void addSkill(UserSkill skill) throws DuplicateSkill
-    {
+    public void addSkill(UserSkill skill) throws DuplicateSkill, SQLException {
         if(skills.containsKey(skill.getName()))
             throw new DuplicateSkill();
         skills.put(skill.getName(), skill);
+        UserSkillMapper.getInstance().insert(skill);
     }
 
     public String getId() {
