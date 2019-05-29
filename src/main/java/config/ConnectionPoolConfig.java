dddb0791cfa922123fcca5cc1e3cc803b69d941d
Farzad Habibi <farzadhabibii98@gmail.com>
2019-05-29 16:34:23 +0430

    update mysql user pass

--- src/main/java/config/ConnectionPoolConfig.java
+++ src/main/java/config/ConnectionPoolConfig.java
@@ -1,9 +1,9 @@
 package config;
 
 public class ConnectionPoolConfig {
-    public static String url = "jdbc:mysql://db:3306/joboonja?useSSL=false";
+    public static String url = "jdbc:mysql://mysql:3306/joboonja?useSSL=false";
     public static String username = "root";
-    public static String password = "13777731";
+    public static String password = "farzad_yasaman";
     public static int minPools = 5;
     public static int maxPools = 10;
     public static int maxOpenPreparedStatement = 100;
