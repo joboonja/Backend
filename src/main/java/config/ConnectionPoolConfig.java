f3f33d9f0f0a4ae66a6429f8ee652abd627950f3
Farzad Habibi <farzadhabibii98@gmail.com>
2019-06-02 15:07:50 +0430

    updata db config
    update db config
    
    update db config
    
    update db config
    
    update db config
    
    update db config

--- src/main/java/config/ConnectionPoolConfig.java
+++ src/main/java/config/ConnectionPoolConfig.java
@@ -1,7 +1,7 @@
 package config;
 
 public class ConnectionPoolConfig {
-    public static String url = "jdbc:mysql://db:3306/joboonja?useSSL=false";
+    public static String url = "jdbc:mysql://185.166.107.169:30718/joboonja?useSSL=false";
     public static String username = "root";
     public static String password = "farzad_yasaman";
     public static int minPools = 5;
