5309b38b2200066f461ed7bbb1cc643dc576e2b2
Farzad Habibi <farzadhabibii98@gmail.com>
2019-06-06 10:40:15 +0430

    disable delay filter

--- src/main/java/controllers/filterDelay/DelayFilter.java
+++ src/main/java/controllers/filterDelay/DelayFilter.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*")
+//@WebFilter("/*")
 public class DelayFilter implements Filter{
     public DelayFilter() {
     }
